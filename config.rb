3fb440963966a783e52a1e7d2d0c1c4887105478
ginmot <gin.motuzyte@gmail.com>
2014-10-31 16:58:37 +0200

    Pridėti SCCS failai ir SCSS konfiguracinis failas

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+
+dir = File.dirname(__FILE__)
+css_dir = "/content/styles"
+images_dir = "/content/images"
+sass_dir = "scss"
+
+output_style = :compact
+relative_assets = true
+line_comments = false
